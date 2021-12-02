9be4824b5474e7d495f518042610d3d4d9ca791e
jackson147 <jaswahhihi@gmail.com>
2021-12-02 08:34:10 +0000

    Day 15 done

--- day15_2020/1/main.go
+++ day15_2020/1/main.go
@@ -8,9 +8,10 @@ import (
 )
 
 type spoken struct {
-	word string
-	position int
-	count int
+	word             string
+	position         int
+	previousPosition int
+	count            int
 }
 
 func check(e error) {
@@ -44,50 +45,49 @@ func main() {
 
 	for i, line := range lines {
 		check(err)
-		valueCounters[line] = spoken{ line, i + 1, 1 }
+		valueCounters[line] = spoken{line, i + 1, -1, 1}
 	}
 
 	numStartingPositions := len(lines)
-	lastSpokenWord := lines[numStartingPositions - 1]
-
+	lastSpokenWord := lines[numStartingPositions-1]
 
 	//Start from next turn after reading in list above
 	i := numStartingPositions + 1
-	for i < 2020 {
-
-		//Map works with string keys
-		//key := strconv.Itoa(i)
+	for i <= 30000000 {
 
 		//Has the last value been spoken more than once?
 		//It will if it exists in the map
-		lastSpoken, exists := valueCounters[lastSpokenWord]
-		//And if the position is greater than the size of the starting list
-		isStartingNumber :=  lastSpoken.position == 1
+		lastSpoken := valueCounters[lastSpokenWord]
 
-		if exists && !isStartingNumber {
+		// fmt.Println("Last number spoken", lastSpokenWord)
+		// fmt.Println(valueCounters)
+
+		if lastSpoken.count == 1 {
+			//The number was spoken for the first time! Speak ZERO for the new word
+			zeroWord := valueCounters["0"]
+			zeroWord.count = zeroWord.count + 1
+			zeroWord.previousPosition = zeroWord.position
+			zeroWord.position = i
+
+			valueCounters["0"] = zeroWord
+			lastSpokenWord = "0"
+		} else if lastSpoken.count > 1 {
 			//The number is being spoken again. Need to shout the distance from the last position
-			check(err)
-			justSpokenWord := strconv.Itoa(i - lastSpoken.position)
+			diff := lastSpoken.position - lastSpoken.previousPosition
+			justSpokenWord := strconv.Itoa(diff)
+			// fmt.Println("DIFFERENCE:", diff, i, lastSpoken.position)
 
-			//Check if this new number exists already. If so update the position to the current positions
-			justSpoken, existsNew := valueCounters[justSpokenWord]
-			if existsNew {
-				justSpoken.position = i
-			}else{//Set a new value
-				valueCounters[justSpokenWord] = spoken{justSpokenWord, i, 1}
-			}
+			justSpoken := valueCounters[justSpokenWord]
+			justSpoken.count = justSpoken.count + 1
+			justSpoken.previousPosition = justSpoken.position
+			justSpoken.position = i
+			valueCounters[justSpokenWord] = justSpoken
 
 			lastSpokenWord = justSpokenWord
-		}else{
-			//The number is new! Shout zero!
-			zeroWord := valueCounters["0"]
-			zeroWord.position = i
-			lastSpokenWord = "0"
 		}
 
-		fmt.Println(i, lastSpokenWord)
+		// fmt.Println(i, lastSpokenWord)
 		i++
 	}
-
-	//fmt.Println(valueCounters)
+	fmt.Println(lastSpokenWord)
 }
