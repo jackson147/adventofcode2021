42cb0980044390045f7256ce793a2af2aaa4e605
jackson147 <jaswahhihi@gmail.com>
2021-12-30 17:13:44 +0000

    Finally completed day14p2, jeeezus

--- day14/2/main.go
+++ day14/2/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"unicode"
 )
@@ -72,14 +73,28 @@ func copyMap(toCopy map[string]int) map[string]int {
 	return newMap
 }
 
+func combineMaps(toAddTo map[string]int, toAdd map[string]int) {
+	for key, value := range toAdd {
+		toAddTo[key] = toAddTo[key] + value
+	}
+}
+
+func printCache(cache map[string]map[string]int) {
+	for key, value := range cache {
+		fmt.Println(key, value)
+	}
+}
+
+var polymerMap map[string]string
+
 func main() {
-	lines, err := readLines("/home/rich/git/advent2021/day14/2/example.txt")
-	// lines, err := readLines("/home/rich/git/advent2021/day14/2/real.txt")
+	// lines, err := readLines("/home/rich/git/advent2021/day14/2/example.txt")
+	lines, err := readLines("/home/rich/git/advent2021/day14/2/real.txt")
 	check(err)
 
 	instructionLine := ""
 
-	polymerMap := make(map[string]string)
+	polymerMap = make(map[string]string)
 
 	for i, line := range lines {
 		if line == "" {
@@ -94,54 +109,93 @@ func main() {
 		}
 	}
 
+	// instructionLine = "CH"
 	fmt.Println(instructionLine)
 	fmt.Println(polymerMap)
 
 	pairs := make([][]string, 0)
 	currentPair := make([]string, 0)
-	resultMap := make(map[string]int)
+	// resultMap := make(map[string]int)
+
+	costMap := make(map[string]int)
 	for _, instructionRune := range instructionLine {
 		currentPair = append(currentPair, string(instructionRune))
 		if len(currentPair) == 2 {
 			pairs = append(pairs, currentPair)
 			currentPair = currentPair[1:]
 		}
-		resultMap[string(instructionRune)] = resultMap[string(instructionRune)] + 1
+		costMap[string(instructionRune)] = costMap[string(instructionRune)] + 1
 	}
 
-	// fmt.Println(resultMap)
-	costMap := make(map[string]int)
-	for i, pair := range pairs {
-		fmt.Println(i)
-		processPairs(polymerMap, resultMap, pair, 0, costMap)
+	totalIterations := 40
+
+	//Prime the cache
+	cache = make(map[string]map[string]int)
+	for i := 0; i < totalIterations; i++ {
+		for polyPair, _ := range polymerMap {
+			cacheCost := make(map[string]int)
+			polyOne := string(polyPair[0])
+			polyTwo := string(polyPair[1])
+			calculateCost(cacheCost, polyOne, polyTwo, i)
+
+			cacheKey := polyOne + polyTwo + strconv.Itoa(i)
+			cache[cacheKey] = copyMap(cacheCost)
+		}
 	}
-	fmt.Println(resultMap)
+
+	for _, pair := range pairs {
+		// fmt.Println(i)
+		calculateCost(costMap, pair[0], pair[1], totalIterations-1)
+	}
+
+	fmt.Println(costMap)
 	values := make([]int, 0)
-	for _, value := range resultMap {
+	for _, value := range costMap {
 		values = append(values, value)
 	}
-	// fmt.Println(values)
+	printCache(cache)
 	min, max := minMax(values)
 	fmt.Println(max - min)
 }
 
-func processPairs(polymerMap map[string]string, resultMap map[string]int, pair []string, iteration int, costMap map[string]int) {
-	combined := pair[0] + pair[1]
-	newValue := polymerMap[combined]
-	resultMap[newValue] = resultMap[newValue] + 1
+var cache map[string]map[string]int
 
-	costMap[newValue] = costMap[newValue] + 1
+func calculateCost(costMap map[string]int, polyOne string, polyTwo string, value int) map[string]int {
+	//Cache map to save
+	cacheCost := make(map[string]int)
 
-	//Create new pairs
-	var newPairOne = []string{pair[0], newValue}
-	var newPairTwo = []string{newValue, pair[1]}
-	iteration++
+	// fmt.Println(polyOne, polyTwo)
+	polyPair := polyOne + polyTwo
 
-	if iteration == 40 {
-		return
+	//Get the new polymer from the inputs
+	newPolymer := polymerMap[polyPair]
+	//Always add the new polymer to the costmap
+	costMap[newPolymer] = costMap[newPolymer] + 1
+	cacheCost[newPolymer] = cacheCost[newPolymer] + 1
+
+	//We're all done, return the cost map
+	if value == 0 {
+		// fmt.Println("RETURN")
+		return costMap
 	}
 
-	//Send for more map entries..
-	processPairs(polymerMap, resultMap, newPairOne, iteration, copyMap(costMap))
-	processPairs(polymerMap, resultMap, newPairTwo, iteration, copyMap(costMap))
+	//Calculate the cost of the new polymers
+	cacheKey := polyOne + newPolymer + strconv.Itoa(value-1)
+	if cachedResult, ok := cache[cacheKey]; ok {
+		combineMaps(costMap, cachedResult)
+	} else {
+		calculateCost(costMap, polyOne, newPolymer, value-1)
+	}
+	// calculateCost(costMap, polyOne, newPolymer, value-1)
+
+	cacheKey = newPolymer + polyTwo + strconv.Itoa(value-1)
+	if cachedResult, ok := cache[cacheKey]; ok {
+		combineMaps(costMap, cachedResult)
+	} else {
+		calculateCost(costMap, newPolymer, polyTwo, value-1)
+	}
+	// calculateCost(costMap, newPolymer, polyTwo, value-1)
+
+	//We'll only get to this point when the the function calls above start reaching value == 0
+	return costMap
 }
